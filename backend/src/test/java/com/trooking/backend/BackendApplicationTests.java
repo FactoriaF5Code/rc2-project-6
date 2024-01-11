3a901fdd4d479d00ee71ab225ac391bda1f80f29
Javi <javi@jsrois.net>
2024-01-11 10:58:17 +0100

    Añadido test de prueba

--- backend/src/test/java/com/trooking/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/trooking/backend/BackendApplicationTests.java
@@ -1,13 +1,30 @@
 package com.trooking.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+	@Autowired
+	private MockMvc api;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+
+		api
+				.perform(get("/api/hello"))
+				.andExpectAll(
+						status().isOk(),
+						content().string(equalTo("Hello!")));
+
 	}
 
 }
